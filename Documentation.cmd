a9bda7012bb5d4451b6584eb08c93c88531c4b6b
Laura Ailinca <119114917+laura2243@users.noreply.github.com>
2023-03-19 00:07:27 +0200

    part1

---
+++ Documentation.cmd
@@ -0,0 +1,23 @@
+# Fitness and Wellness Application 
+
+## Introduction
+The application is designed to keep track of users' workouts, create custom workouts composed from different types of exercises, and give recommendations to new users based on their height, age and kilograms. 
+The Fitness Software provides lots of functions such as data entry of customers, keeping records of all the things about customers' entire journey within the application, and physical fitness which help to provide good quality of services.
+The application has two types of users:
+1. **Standard user** ( this type of account is made automatically through registration and it has access to standard features )
+2. **Admin** ( it has access to all existent accounts and to extended features such as accessing the database of the application and all its users )
+## Objectives
+- The main objective of the project is to develop software that facilitates the
+data storage, data maintenance and its retrieval for the customer in an igneous way.
+- To store the record of the customers, the staff that has the privileges to access, modify and delete any record and finally the service.
+- Also, only the staff has the privilege to access any database and make the
+required changes, if necessary.
+- To develop easy-to-use software which handles the customer-staff
+relationship in an effective manner.
+- To develop a user friendly system that requires minimal user
+training. Most of the features and function are similar to those on any windows platform.
+
+## Functionalities
+#### 1.Register
+
+
